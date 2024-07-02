5f2e2968c7d52f3a7a27492e020ff5f2b4ae1d3f
TLP-264 <harel.mael@mail.huji.ac.il>
2024-07-02 09:58:32 +0300

    stupis gay itamar

--- MapReduceFramework.cpp
+++ MapReduceFramework.cpp
@@ -1,4 +1,23 @@
 #include "MapReduceFramework.h"
+#include <pthread.h>
+
+
+typedef struct{
+    JobState *job_state;
+    pthread_t threads[];
+} JobData;
+
+void emit2 (K2* key, V2* value, void* context){
+
+
+}
+
+
+void getJobState(JobHandle job, JobState* state){
+  JobData* jb = (JobData*) job;
+  jb->job_state->percentage = state->percentage;
+  jb->job_state->stage = state->stage;
+}
 
 
 
