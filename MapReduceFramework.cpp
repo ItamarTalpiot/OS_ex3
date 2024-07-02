e49f29fdab3a7a5cb7c714da1a06acfa3b450e5c
hmey842 <harel.mael.44@gmail.com>
2024-07-02 09:15:19 +0300

    cmake

--- MapReduceFramework.cpp
+++ MapReduceFramework.cpp
@@ -1,2 +1,7 @@
 #include "MapReduceFramework.h"
 
+
+
+
+
+
---
+++ cmake-build-debug/CMakeFiles/OS_ex3.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OS_ex3.dir/Atomic/atomicdemo_multivar.cpp.o"
+  "CMakeFiles/OS_ex3.dir/Atomic/atomicdemo_multivar.cpp.o.d"
+  "OS_ex3"
+  "OS_ex3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OS_ex3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
